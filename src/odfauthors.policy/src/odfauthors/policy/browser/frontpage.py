5e27e594135f7fb46fd67a9edbaac3de3bb897c0
andreasma <maand@gmx.de>
2018-06-12 20:30:01 +0200

    fixed an import

--- src/odfauthors.policy/src/odfauthors/policy/browser/frontpage.py
+++ src/odfauthors.policy/src/odfauthors/policy/browser/frontpage.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from plone import api
 from Products.Five.browser import BrowserView
-from tdf.exttempsitepolicy import _
+from odfauthors.policy import _
 from Acquisition import aq_inner
 
 
