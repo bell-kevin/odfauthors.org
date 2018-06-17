a403d45e87b8ec3190ae93f1aa17f0bd0a0e9136
andreasma <maand@gmx.de>
2018-06-17 19:44:43 +0200

    add testing module to package

---
+++ src/odfauthors.policy/src/odfauthors/policy/testing.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import helpers
+
+import odfauthors.policy
+import collective.MockMailHost
+
+
+class OdfAutthorspolicyLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        self.loadZCML(package=odfauthors.policy)
+        self.loadZCML(package=collective.MockMailHost)
+        z2.installProduct(app, 'collective.MockMailHost')
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'odfauthors.policy:default')
+        applyProfile(portal, 'collective.MockMailHost:default')
+        helpers.quickInstallProduct(portal, 'collective.MockMailHost')
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+
+    def tearDownZope(self, app):
+        # Uninstall product
+        z2.uninstallProduct(app, 'collective.MockMailHost')
+
+
+ODFAUTHORS_POLICY_FIXTURE = OdfAuthorspolicyLayer()
+
+
+ODFAUTHORS_POLICY_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(ODFAUTHORS_POLICY_FIXTURE,),
+    name='OdfAuthorspolicyLayer:IntegrationTesting'
+)
+
+
+ODFAUTHORS_POLICY_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(ODFAUTHORS_POLICY_FIXTURE,),
+    name='OdfAuthorspolicyLayer:FunctionalTesting'
+)
+
+
+ODFAUTHORS_POLICY_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        ODFAUTHORS_POLICY_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE
+    ),
+    name='OdfAuthorspolicyLayer:AcceptanceTesting'
+)
